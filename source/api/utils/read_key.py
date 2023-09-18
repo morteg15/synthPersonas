9e235aaefd8fdd6cdb9008b0907f4f939684b4d4
torjerb <torjer.bertelsen@gmail.com>
2023-09-18 13:30:55 +0200

    commit

--- source/api/utils/read_key.py
+++ source/api/utils/read_key.py
@@ -1,19 +1,23 @@
 import os
 import configparser
-
+import openai
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
 
 def read_key():
+    # Get the absolute path to the configuration file
+    config_file_path = os.path.join(os.path.dirname(__file__), 'source', 'api', 'utils', 'config.ini')
+
     # Initialize the configparser
     config = configparser.ConfigParser()
 
-    # Read the configuration file
-    config.read('source\\api\\utils\\config.ini')
+    # Read the configuration file using the absolute file path
+    config.read(config_file_path)
+
+    openai.api_key = os.getenv('OPENAI_API_KEY')
 
     # Get the API key from the configuration file
-    api_key = config['OpenAI']['API_KEY']
+    print(os.getenv('OPENAI_API_KEY'))
 
-    # Set the API key as an environment variable
-    os.environ['OPENAI_API_KEY'] = api_key
-
-    # Now you can use os.getenv("OPENAI_API_KEY") to get the API key
-    print(os.getenv("OPENAI_API_KEY"))
+# Call the function to read the API key
+read_key()
